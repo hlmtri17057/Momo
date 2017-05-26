4b034cd2fb633653d55d94a26480d43f65e89573
Huỳnh Lê Minh Trí <hlmtri@mac09.local>
2017-05-26 15:55:16 +0700

    Initial Commit

---
+++ MomoappV2Tests/MomoappV2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  MomoappV2Tests.swift
+//  MomoappV2Tests
+//
+//  Created by Huỳnh Lê Minh Trí on 5/26/17.
+//  Copyright © 2017 cusc. All rights reserved.
+//
+
+import XCTest
+@testable import MomoappV2
+
+class MomoappV2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
